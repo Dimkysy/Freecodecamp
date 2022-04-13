038ac321f807ec44c7125e2b63903038b74ca6eb
Dimkysy <diman.nesterov1995@gmail.com>
2022-04-13 19:22:37 +0300

    Freecodecamp all

---
+++ Build JavaScript Objects.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    name: "Hello",
+    legs : 2,
+    tails : 3,
+    friends : [1,2,3]
+
+    // Only change code above this line
+};
---
+++ Iterate with JavaScript While Loops.js
@@ -0,0 +1,14 @@
+// Setup
+const myArray = [];
+
+let i = 5 ;
+
+while (i <= 5 ) {
+    if (i >= 0) {
+        myArray.push(i)
+        i--
+    }
+}
+
+
+// Only change code below this line
